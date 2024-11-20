85eb028f6ff0b0c342ea343083bcd5bb3b954a4c
piyush-tecnobelite <159113727+piyush-tecnobelite@users.noreply.github.com>
2024-11-20 09:52:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -89,7 +89,7 @@ app.use("/", newsletterRoutes);
 
 // Starting Server
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 
 app.listen(port, () => {
   console.log("Server running on port", port);
